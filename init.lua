7cf964c2a32b3402dd2f5df6ffe427b10c894ba6
Jón Velasksson <80118997+Ljosilfr@users.noreply.github.com>
2026-01-11 23:37:31 -0400

    Add files via upload

--- init.lua
+++ init.lua
@@ -2,10 +2,13 @@ vim.opt.number = true
 vim.opt.termguicolors = true
 vim.opt.linebreak = true
 vim.cmd('syntax on')
+vim.cmd('set nowrap')
+vim.cmd('set clipboard=unnamedplus')
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.keymap.set('n', '<F9>', ':w<CR>', { desc = 'Save current file' })
+vim.keymap.set('n', '<F8>', ':wq<CR>', { desc = 'Save current file' })
 
 require("config.lazy")

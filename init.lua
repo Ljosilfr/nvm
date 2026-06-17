fa70fa7f56f82b48e19286251f82a2a243ebae7b
Jón Velasksson <80118997+Ljosilfr@users.noreply.github.com>
2026-06-17 18:21:28 -0400

    Add files via upload

--- init.lua
+++ init.lua
@@ -7,8 +7,18 @@ vim.cmd('set clipboard=unnamedplus')
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
 
 vim.keymap.set('n', '<F9>', ':w<CR>', { desc = 'Save current file' })
 vim.keymap.set('n', '<F8>', ':wq<CR>', { desc = 'Save current file' })
-
 require("config.lazy")
